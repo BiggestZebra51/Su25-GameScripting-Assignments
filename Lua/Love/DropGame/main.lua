064cdc7bcec340ebbc081d470de4968f14305344
BiggestZebra51 <217064964+BiggestZebra51@users.noreply.github.com>
2025-07-01 07:11:25 -0500

    Rebrand to ufos and add a bomb powerup

--- Lua/Love/DropGame/main.lua
+++ Lua/Love/DropGame/main.lua
@@ -10,8 +10,7 @@ end
 -- should have title screen, level 1, game over
 
 function titleLoad()
-    slimeIdle = love.graphics.newImage("sprites/slime/slime_idle.png")
-    titleText = "Slime Drop"
+    titleText = "UFO Drop"
 
     -- Update the window to be titled titleText
     love.window.setTitle(titleText)
@@ -33,35 +32,44 @@ function love.load()
     -- 2 = game over screen
     scene = 1
 
-    slimeIdle = love.graphics.newImage("sprites/slime/slime_idle.png")
-    slimeNums = 5 -- how many
-    slimeX = {} -- Where at (x)
-    slimeY = {} -- Where at (y)
-    slimeSpeed = {} -- How fast
-    minSpeed = 1
-    maxSpeed = 20
-    speedMod = 1
-    count = slimeNums
+    ufoSprite = love.graphics.newImage("sprites/ufo/ufo_green.png")
+    ufoNums = 5 -- how many
+    ufoX = {} -- Where at (x)
+    ufoY = {} -- Where at (y)
+    ufoSpeed = {} -- How fast
+
+    minSpeed = 5        -- The min speed for the ufos to move
+    maxSpeed = 20       -- The max speed for the ufos to move
+    maxSpeedOffset = 1  -- The max speed + (this offset incremented each click)
+
+    globalSpeedMod = 1  -- A multiplier to the global speed
+
+    bombSprite = love.graphics.newImage("sprites/items/bomb.png")
+    powerupVisible = false
+    powerupX = 0 -- Where at (x)
+    powerupY = 0 -- Where at (y)
+
+    powerupCooldown = 3               -- The cool down before the powerup can spawn again
+    powerupSpawnOffsetMax = 4         -- The max offset for the cooldown
+    powerupSpawnOffset = 0            -- The randomized offset for the powerup spawn cooldown
+    powerupLastActivatedTime = love.timer.getTime() -- The time the powerup was last activated
 
     -- Randomization
     math.randomseed(os.time())
     math.random(); math.random(); math.random()
 
-    -- Initially populate the slimes
-    while count > 0 do
-        slimeX[#slimeX+1] = math.random(0, love.graphics.getWidth() - slimeIdle:getWidth())
-        -- get a random y value between 1 and 2 slimes above window
-        slimeY[#slimeY+1] = 0 - math.random(slimeIdle:getHeight(), slimeIdle:getHeight()*2)
+    for i = 1, ufoNums, 1 do
+        ufoX[#ufoX+1] = math.random(0, love.graphics.getWidth() - ufoSprite:getWidth())
+        -- get a random y value between 1 and 2 ufos above window
+        ufoY[#ufoY+1] = 0 - math.random(ufoSprite:getHeight(), ufoSprite:getHeight()*2)
         -- get a random speed between min and max
-        slimeSpeed[#slimeSpeed+1] = math.random(minSpeed, maxSpeed)
-
-        count = count - 1 -- count down
+        ufoSpeed[#ufoSpeed+1] = math.random(minSpeed, maxSpeed)
     end
 end
 
 --------------------------------------------------------------------------
 -- Click handling
--- 
+--
 --------------------------------------------------------------------------
 function love.mousepressed(x, y, button, istouch)
     -- if it's left click
@@ -72,21 +80,43 @@ function love.mousepressed(x, y, button, istouch)
         end
         -- in game
         if scene == 1 then
-            -- Check each image and see if it has collided with the mouse click
-            for i, value in pairs(slimeX) do
-                if x >= slimeX[i] and x <= slimeX[i] + slimeIdle:getWidth()
-                   and y >= slimeY[i] and y <= slimeY[i] + slimeIdle:getHeight() then
-                    -- send back to top and change speed
-                    speedMod = speedMod + 1
-                    maxSpeed = maxSpeed + speedMod
+            -- If the bomb was pressed, clear all ufos and re-randomize them
+            if powerupVisible
+                    and x >= powerupX and x <= powerupX + bombSprite:getWidth()
+                    and y >= powerupY and y <= powerupY + bombSprite:getHeight() then
 
-                    slimeX[i] = math.random(0, love.graphics.getWidth() - slimeIdle:getWidth())
-                    -- get a random y value between 1 and 2 slimes above window
-                    slimeY[i] = 0 - math.random(slimeIdle:getHeight(), slimeIdle:getHeight()*2)
-                    -- get a random speed between min and max
-                    slimeSpeed[i] = math.random(slimeSpeed[i], maxSpeed)
+                powerupVisible = false -- Hide the powerup
+                powerupLastActivatedTime = love.timer.getTime()         -- The time the powerup was last activated
+                powerupSpawnOffset = math.random(powerupSpawnOffsetMax) -- Set the timer offset for next spawn
 
-                    break
+                -- send back to top and change speed
+                maxSpeedOffset = maxSpeedOffset + 1
+                maxSpeed = maxSpeed + maxSpeedOffset
+
+                for i, value in pairs(ufoX) do
+                        ufoX[i] = math.random(0, love.graphics.getWidth() - ufoSprite:getWidth())
+                        -- get a random y value between 1 and 2 ufos above window
+                        ufoY[i] = 0 - math.random(ufoSprite:getHeight(), ufoSprite:getHeight()*2)
+                        -- get a random speed between min and max
+                        ufoSpeed[i] = math.random(ufoSpeed[i], maxSpeed)
+                end
+            else
+                -- Check each image and see if it has collided with the mouse click
+                for i, value in pairs(ufoX) do
+                    if x >= ufoX[i] and x <= ufoX[i] + ufoSprite:getWidth()
+                    and y >= ufoY[i] and y <= ufoY[i] + ufoSprite:getHeight() then
+                        -- send back to top and change speed
+                        maxSpeedOffset = maxSpeedOffset + 1
+                        maxSpeed = maxSpeed + maxSpeedOffset
+
+                        ufoX[i] = math.random(0, love.graphics.getWidth() - ufoSprite:getWidth())
+                        -- get a random y value between 1 and 2 ufos above window
+                        ufoY[i] = 0 - math.random(ufoSprite:getHeight(), ufoSprite:getHeight()*2)
+                        -- get a random speed between min and max
+                        ufoSpeed[i] = math.random(ufoSpeed[i], maxSpeed)
+
+                        break
+                    end
                 end
             end
         end
@@ -95,17 +125,35 @@ end
 
 --------------------------------------------------------------------------
 -- Update
--- 
+--
 --------------------------------------------------------------------------
 function love.update(dt)
     -- GAMEPLAY
     if scene == 1 then
-        for i, yPos in ipairs(slimeY) do
-            if slimeY[i] + slimeIdle:getHeight() >= love.graphics.getHeight() then
+        for i, yPos in ipairs(ufoY) do
+            if ufoY[i] + ufoSprite:getHeight() >= love.graphics.getHeight() then
                 love.event.quit()
             end
-            -- Move slimes
-            slimeY[i] = yPos + (slimeSpeed[i] * dt)
+            -- Move ufos
+            ufoY[i] = yPos + (ufoSpeed[i] * dt * globalSpeedMod)
+        end
+
+        local timeSinceActivated = love.timer.getTime() - powerupLastActivatedTime
+        
+        if timeSinceActivated > powerupCooldown then
+            -- Hide the powerup after having been visible for (cooldown) seconds
+            if powerupVisible then
+                powerupVisible = false -- Hide the powerup
+                powerupLastActivatedTime = love.timer.getTime()         -- The time the powerup was last activated
+                powerupSpawnOffset = math.random(powerupSpawnOffsetMax) -- Set the timer offset for next spawn
+
+            elseif timeSinceActivated > (powerupCooldown + powerupSpawnOffset) then 
+                powerupX = math.random(0, love.graphics.getWidth() - bombSprite:getWidth())
+                powerupY = math.random(0, love.graphics.getHeight() - bombSprite:getHeight())
+
+                powerupVisible = true -- Show the powerup
+                powerupLastActivatedTime = love.timer.getTime() -- The time the powerup was last activated
+            end
         end
     end
 end
@@ -121,8 +169,12 @@ function love.draw()
     end
     -- GAMEPLAY
     if scene == 1 then
-        for i, xPos in ipairs(slimeX) do
-            love.graphics.draw(slimeIdle, xPos, slimeY[i])
+        if powerupVisible then
+            love.graphics.draw(bombSprite, powerupX, powerupY)
+        end
+
+        for i, xPos in ipairs(ufoX) do
+            love.graphics.draw(ufoSprite, xPos, ufoY[i])
         end
     end
     -- GAMEOVER
